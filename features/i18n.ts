97ec901abc7a3edb62f6552a08e57ea4ac6889d8
Antares <antares.mi@outlook.com>
2026-07-09 16:34:06 +0800

    fix

--- features/i18n.ts
+++ features/i18n.ts
@@ -1,30 +1,32 @@
 import type { Context } from "hydrooj";
 
+import { ROUTE_BLOG_DETAIL, ROUTE_BLOG_EDIT, ROUTE_BLOG_LIST_USER } from "./handler";
+
 export function applyI18n(ctx: Context) {
     ctx.i18n.load("zh", {
         "{0}'s blog": "{0} 的博客",
         Blog: "博客",
-        blog_detail: "博客详情",
-        blog_edit: "编辑博客",
-        blog_main: "博客",
+        [ROUTE_BLOG_DETAIL]: "博客详情",
+        [ROUTE_BLOG_EDIT]: "编辑博客",
+        [ROUTE_BLOG_LIST_USER]: "博客",
     });
     ctx.i18n.load("zh_TW", {
         "{0}'s blog": "{0} 的部落格",
         Blog: "部落格",
-        blog_detail: "部落格詳情",
-        blog_edit: "編輯部落格",
-        blog_main: "部落格",
+        [ROUTE_BLOG_DETAIL]: "部落格詳情",
+        [ROUTE_BLOG_EDIT]: "編輯部落格",
+        [ROUTE_BLOG_LIST_USER]: "部落格",
     });
     ctx.i18n.load("kr", {
         "{0}'s blog": "{0}의 블로그",
         Blog: "블로그",
-        blog_main: "블로그",
-        blog_detail: "블로그 상세",
-        blog_edit: "블로그 수정",
+        [ROUTE_BLOG_LIST_USER]: "블로그",
+        [ROUTE_BLOG_DETAIL]: "블로그 상세",
+        [ROUTE_BLOG_EDIT]: "블로그 수정",
     });
     ctx.i18n.load("en", {
-        blog_main: "Blog",
-        blog_detail: "Blog Detail",
-        blog_edit: "Edit Blog",
+        [ROUTE_BLOG_LIST_USER]: "Blog",
+        [ROUTE_BLOG_DETAIL]: "Blog Detail",
+        [ROUTE_BLOG_EDIT]: "Edit Blog",
     });
 }

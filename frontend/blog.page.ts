8ce6560d6c783c7d5636875b247a4fed275225f7
Antares <antares.mi@outlook.com>
2026-07-19 19:47:44 +0800

    fix

--- frontend/blog.page.ts
+++ frontend/blog.page.ts
@@ -5,7 +5,14 @@ addPage(
         $("<a>")
             .append($("<span>").addClass("icon icon-book"))
             .attr("data-tooltip", i18n("Blog"))
-            .attr("href", `/blog/${UiContext.udoc._id}`) // eslint-disable-line @typescript-eslint/no-unsafe-member-access
+            .attr(
+                "href",
+                UiContext.domainId === "system"
+                    ? // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+                      `/blog/${UiContext.udoc._id}`
+                    : // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+                      `/d/${UiContext.domainId}/blog/${UiContext.udoc._id}`,
+            )
             .addClass("profile-header__contact-item")
             .insertBefore($('a.profile-header__contact-item[href*="/home/messages"]'));
     }),
